fb09d2d3240783b6448e25dfbe9f46f1317118d6
renqwee <xrayan.23@gmail.com>
2026-08-02 04:14:42 +0300

    docs: amazon is implemented; bump to 0.4.0

--- productspy/__init__.py
+++ productspy/__init__.py
@@ -19,7 +19,7 @@ from .utils.url_tools import extract_domain
 
 from . import trackers as _trackers  # noqa: F401
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 __all__ = [
     "get_product_info",
